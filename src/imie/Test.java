4c6c3a7b6ff60937bdb67ad5f8fd51f7383f9ca1
Girault <jgirault@Packard-Ubuntu.(none)>
2016-11-09 11:32:10 +0100

    Ajout du menu3 avec jeu mystere et convertisseur.

--- src/imie/Test.java
+++ src/imie/Test.java
@@ -1,5 +1,6 @@
 package imie;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Test {
@@ -8,7 +9,8 @@ public class Test {
 	{		
 		int a = 1; //ma modif
 		
-		notes();
+		menu3();
+		//notes();
 		//ajout de ça !
 	}
 	
@@ -68,7 +70,7 @@ public class Test {
 		int choix = 1;
 		while(choix != 4)
 		{
-			System.out.println("######### MENU ##########");
+			System.out.println("\n######### MENU ##########");
 			System.out.println("1 - Addition");
 			System.out.println("2 - Soustraction");
 			System.out.println("3 - Multiplication");
@@ -133,9 +135,92 @@ public class Test {
 		}
 	}
 	
+	public static void menu3()
+	{
+		Scanner sc = new Scanner(System.in);
+		int choix;
+
+		do
+		{
+			//Affichage du menu
+			System.out.println("\n########## MENU ############");
+			System.out.println("1 - Jeu du nombre mystère");
+			System.out.println("2 - Convertisseurs miles en km");
+			System.out.println("3 - Quitter");
+
+			System.out.print("Choisir une option : ");
+			do{
+				choix = sc.nextInt();
+				if(choix < 1 || choix > 3)
+					System.out.print("Option invalide, saisir un chiffre entre 1 et 3 : ");
+			}while(choix < 1 || choix > 3);
+
+			//Traitement du choix
+			switch (choix) {
+				case 1:
+					jeuNombreMystere();
+					break;
+				case 2:
+					conversionMilesKm();
+					break;
+				default:
+					break;
+			}
+			
+		}while(choix != 3);
+		
+		System.out.println("Bye bye !");
+	}
 	
+	public static void jeuNombreMystere()
+	{
+		int nombrePropose, nombreMystere, nbTentatives;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n\n####### JEU DU NOMBRE MYSTERE #######");
+		System.out.println("But du jeu : trouver le nombre mystère (entre 1 et 1000) le plus rapidement possible.");
+		System.out.println("Tapez 0 si vous souhaitez abandonner.");
+		
+		//Initialisation du nombre mystere
+		Random rand = new Random();
+		nombreMystere = rand.nextInt(1000)+1;
+		nbTentatives = 0;
+		
+		do{
+			System.out.print("Proposez un nombre : ");
+			nombrePropose = sc.nextInt();
+			nbTentatives++;
+			
+			//Je verifie que le joueur ne veut pas abandonner
+			if(nombrePropose != 0)
+			{
+				if(nombrePropose < 1 || nombrePropose > 1000) {
+					System.out.println("Vous devez choisir un nombre entre 1 et 1000 !");
+					nbTentatives--; //on annule cette tentative
+				}
+				else if(nombrePropose < nombreMystere)
+					System.out.println("C'est + !");
+				else if(nombrePropose > nombreMystere)
+					System.out.println("C'est moins !");
+			}
+			
+		}while(nombreMystere != nombrePropose && nombrePropose != 0);
+		
+		if(nombreMystere == nombrePropose)
+			System.out.println("Félicitations, vous avez gagné ! Vous avez trouvé le nombre mystère en " + nbTentatives + " tentatives.");
+		else
+			System.out.println("Vous êtes un GROS NUL ! Bye.");
+	}
 	
-	
-	
+	public static void conversionMilesKm()
+	{
+		Scanner sc = new Scanner(System.in);
+		double miles;
+
+		System.out.println("\n\n####### CONVERTISSEUR #######");
+		System.out.print("Entrez le nombre de miles à convertir en kilomètres : ");
+		miles = sc.nextDouble();
+		
+		System.out.println(miles + " miles ==> " + (miles*1.60934) + " km");
+	}
 	
 }
